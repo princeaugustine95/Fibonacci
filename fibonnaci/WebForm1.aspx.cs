c5c65e4b57ed660ff41a7795675e342f3081f9ec
Prince augustine <princeaugustine95@gmail.com>
2019-01-27 19:08:01 -0500

    Importing Project

---
+++ fibonnaci/WebForm1.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace fibonnaci
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int num = Convert.ToInt32(TextBox1.Text);
+            int a = 0, b = 1, c = 0;
+            Response.Write(a);
+            Response.Write(b);
+            for(int i=2;i<num;i++)
+            {
+                c = a + b;
+                Response.Write(c);
+                a = b;
+                b = c;
+            }
+
+        }
+    }
+}
